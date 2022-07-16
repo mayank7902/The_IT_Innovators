23546bf832da7a8f2735d28f8b90dbcbcd3f1431
mayank7902 <mayankchordiya7@gmail.com>
2022-07-16 12:19:10 +0530

    test

---
+++ app/src/main/java/com/example/the_it_innovators/Mayank1.java
@@ -0,0 +1,14 @@
+package com.example.the_it_innovators;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Mayank1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
